44a87f99c1bde34f7492b32d96bec08cf9029209
rrr <158373993+Outerstellar1@users.noreply.github.com>
2024-12-12 10:06:21 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,7 +1,7 @@
 
 self.__uv$config = {
     prefix: '/static/ixl/',
-    bare:'https://collegeapp.me/v/',
+    bare:'https://bare.chromehack.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
